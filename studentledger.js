2124d70462541acf55efe6afb741cd25d2b138eb
Jommar Ilagan <jom102386@gmail.com>
2020-07-15 15:07:04 +0800

    added or viewing

--- studentledger.js
+++ studentledger.js
@@ -1,23 +1,42 @@
-$(document).ready(function(){
-    (function($){
-        $.fn.search_student = function(){
-            var $obj = this;
-            var data = {
-                form:$obj.serializeArray()
-            };
-            $obj.closest('.row').find('.op').html(jom.loading);
-            $.post(`${$obj.attr('data--url')}`,{data:data},function(output){
-                $obj.closest('.row').find('.op').html(output);
-                console.log(output)
-            })
-        }
-    }(jQuery));
+$(document).ready(function () {
+  (function ($) {
+    $.fn.search_student = function () {
+      var $obj = this;
+      var data = {
+        form: $obj.serializeArray(),
+      };
+      $obj.closest(".row").find(".op").html(jom.loading);
+      $.post(`${$obj.attr("data--url")}`, { data: data }, function (output) {
+        $obj.closest(".row").find(".op").html(output);
+        console.log(output);
+      });
+    };
+  })(jQuery);
 
-    (function($){
-        $.fn.replaceNameByCode = function(){
-            var $obj = this;
-            $obj.closest('.row').find('[name="sn"]').val($obj.attr('data--code'));
-            $obj.closest('.row').find('form').search_student();
-        }
-    }(jQuery))
-})
+  (function ($) {
+    $.fn.search_student_payments = async function () {
+      // const $obj = this;
+      // const data = {
+      //   name: $obj.find('[name="name"]').val(),
+      //   orno: $obj.find('[name="orno"]').val(),
+      // };
+      // const url = `${$obj.attr('data--url')}payments/search?auth=${$obj.attr('data--key')}&name=${data.name}&orno=${data.orno}`;
+
+      // const response = await fetch(
+      //   url,
+      //   {method:'GET'}
+      // );
+      // const responseJson = await response.json();
+      
+      // console.log(responseJson);
+    };
+  })(jQuery);
+
+  (function ($) {
+    $.fn.replaceNameByCode = function () {
+      var $obj = this;
+      $obj.closest(".row").find('[name="sn"]').val($obj.attr("data--code"));
+      $obj.closest(".row").find("form").search_student();
+    };
+  })(jQuery);
+});
